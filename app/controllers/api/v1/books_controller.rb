bf7b08f0c46b5a328948d2f0e103e9d97304a057
sturpin <turpiniko@gmail.com>
2021-10-03 22:47:57 +0200

    Creating multiple records in Controllers

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -9,7 +9,8 @@ module Api
       end
 
       def create
-        book = Book.new(book_params)
+        author = Author.create(author_params)
+        book = Book.new(book_params.merge(author_id: author.id))
 
         if book.save
           render json: book, status: :created
@@ -24,6 +25,10 @@ module Api
       end
 
       private
+
+      def author_params
+        params.require(:author).permit(:first_name, :last_name, :age)
+      end
 
       def book_params
         params.require(:book).permit(:title, :author)
