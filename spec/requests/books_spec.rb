3d2d177000b1d6fdb348140241bf180bccefd70e
sturpin <turpiniko@gmail.com>
2021-11-01 20:35:52 +0100

    Adding an API response body test, for a single object

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -16,13 +16,21 @@ describe 'Books API', type: :request do
         it 'create a new book' do
             expect {
                 post '/api/v1/books', params: { 
-                    book: { title: 'The martian'},
+                    book: {title: 'The Martian'},
                     author: {first_name: 'Andy', last_name: 'Weir', age: '48'}
                 }
             }.to change { Book.count }.from(0).to(1)
 
             expect(response).to have_http_status(:created)
             expect(Author.count).to eq(1)
+            expect(JSON.parse(response.body)).to eq(
+                {
+                    'id' => 1,
+                    'title' => 'The Martian',
+                    'author_name' => 'Andy Weir',
+                    'author_age' => 48
+                }
+            )
         end
     end
 
